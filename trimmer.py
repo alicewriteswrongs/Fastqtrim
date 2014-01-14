93c4428b1ef46a1f0466cc794f57a784b0382718
benpote <ben.pote@gmail.com>
2014-01-14 13:03:22 -0500

    Updated trimmer.py with license details and excluded .swp in .gitignore

--- trimmer.py
+++ trimmer.py
@@ -8,6 +8,20 @@ Created by Ben Pote on 1/14/14
 The author may be contacted at ben.pote@gmail.com
 
 Takes a fastq file and trims off a certain number of lines.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 
 import gzip
