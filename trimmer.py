062e7b6697d3e5f4907fdad99f2a0a1ba895cf9c
benpote <ben.pote@gmail.com>
2014-01-14 12:50:23 -0500

    Updated trimmer.py and .gitignore

--- trimmer.py
+++ trimmer.py
@@ -10,19 +10,55 @@ The author may be contacted at ben.pote@gmail.com
 Takes a fastq file and trims off a certain number of lines.
 """
 
+import gzip
 import argparse
+import sys
 
+#####get arguments
 def get_args():
-	""" parse sys.argv """
-	#initiate parser	
-	parser = argparse.ArgumentParser()
-	#add commandline argument
-	parser.add_argument('-s','--start',
-		required=False,
-		type=int,
-		help='Trim x number of bases from the start')
-	#process arguments
-	args = parser.parse_args()
-	return args
+    """ parse sys.argv """
+    #initiate parser	
+    parser = argparse.ArgumentParser()
+    #add commandline argument
+    parser.add_argument('-s','--start',
+        required=False,
+        type=int,
+        help='Trim x number of bases from the start')
 
-get_args
+    parser.add_argument('-e','--end',
+        required=False,
+        type=int,
+        help='Trim x number of bases from the end')
+
+    parser.add_argument('INPUT',
+        type=argparse.FileType('rb'),
+        default=sys.stdin,
+        nargs='?',
+        help='VCF file')
+
+    parser.add_argument('OUTPUT',
+        type=argparse.FileType('w'),
+        default=sys.stdout,
+        nargs='?',
+        help='VCF file')
+    #process arguments
+    args = parser.parse_args()
+    return args
+
+
+#### main function
+def main(args):
+    start = args.start
+    fin = enumerate(args.INPUT)
+    fout = args.OUTPUT
+    end = args.end
+    for line in fin:
+        count, text = line
+        print text[start:end]
+
+
+
+
+if __name__ == '__main__':
+	args = get_args()
+	main(args)
