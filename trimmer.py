1c4c8ca5571ae264a0d1003cf6bd09eb8a2823c9
benpote <ben.pote@gmail.com>
2014-01-14 10:59:24 -0500

    initial commit

---
+++ trimmer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: etf-8
+
+"""
+trimmer.py
+Created by Ben Pote on 1/14/14
+
+The author may be contacted at ben.pote@gmail.com
+
+Takes a fastq file and trims off a certain number of lines.
+"""
+
+import argparse
